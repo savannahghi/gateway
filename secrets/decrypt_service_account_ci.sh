00d8d69cbbc0aa10e743a8a7d7a203ad783fcb54
Otieno Calvine <nyarangaotieno@gmail.com>
2021-08-16 11:44:42 +0300

    fix deployment
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- secrets/decrypt_service_account_ci.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Decrypt the service account file
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output /opt/gateway/bewell-app-ci.json bewell-app-ci.json.gpg
